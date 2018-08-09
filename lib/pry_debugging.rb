0b44c538133954a6043b8775ed617650278826e1
Ricky Liu <rorix12321@gmail.com>
2018-08-09 03:42:06 +0000

    automatically backed up by learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,7 +1,5 @@
-From:Users/sophiedebenedetto/Desktop/Dev/Ruby-Methods_and_Variables/pry-readme/lib/pry_debugging.rb @ line 4 Object#plus_two:
-
-    1: def plus_two(num)
-    2:  num + 2
-    3:  num
- => 4:  binding.pry
-    5: end
+def plus_two(num)
+    num + 2
+    num
+    binding.pry
+end
