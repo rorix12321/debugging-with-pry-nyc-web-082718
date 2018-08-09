835585c1d6cdb08243b22419bf341c999be294b7
Ricky Liu <rorix12321@gmail.com>
2018-08-09 03:41:34 +0000

    automatically backed up by learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,4 @@
-From: Users/sophiedebenedetto/Desktop/Dev/Ruby-Methods_and_Variables/pry-readme/lib/pry_debugging.rb @ line 4 Object#plus_two:
+From:Users/sophiedebenedetto/Desktop/Dev/Ruby-Methods_and_Variables/pry-readme/lib/pry_debugging.rb @ line 4 Object#plus_two:
 
     1: def plus_two(num)
     2:  num + 2
